bb0db8a6305ed3fbf274d4d5fca8dc717f3efa5b
sakeer <sakeerpkn@gmail.com>
2016-01-05 15:17:34 +0530

    updated

--- docker_django/settings.py
+++ docker_django/settings.py
@@ -76,11 +76,14 @@ WSGI_APPLICATION = 'docker_django.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql', 
+        'NAME': 'blog',
+        'USER': 'root',
+        'PASSWORD': 'sakeer123s',
+        'HOST': 'db',   # Or an IP Address that your DB is hosted on
+        'PORT': '3306',
     }
 }
-
 
 # Internationalization
 # https://docs.djangoproject.com/en/1.8/topics/i18n/
